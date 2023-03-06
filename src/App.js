55540286321e6bde1e7be5e0a6d35209768f14bd
Chris <f.chrisgoodson@icloud.com>
2023-03-06 00:32:44 -0600

    built

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GraphTable from './GraphTable';
+import { collectData } from './binarySearchTree';
 
 function App() {
+  const data = collectData();
+
+  const trace1 = {
+    x: data.map(d => d.n),
+    y: data.map(d => d.height / (2 * Math.log(d.n))),
+    type: 'scatter',
+    name: 'h(n) / 2lg(n)',
+    
+
+  };
+
+  const trace2 = {
+    x: data.map(d => d.n),
+    y: data.map(d => d.height / ((2 * d.n) ** 0.5)),
+    type: 'scatter',
+    name: 'h(n) / 2n√n)',
+  };
+
+  const trace3 = {
+    x: data.map(d => d.n),
+    y: data.map(d => d.height / ((2 * d.n) * Math.log(d.n))),
+    type: 'scatter',
+    name: 'h(n) / 2nlg(n)',
+  };
+
+  const tableData1 = data.map(d => [d.n, d.height, (d.height / (2 * Math.log(d.n))).toFixed(4)]);
+  const tableData2 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) ** 0.5)).toFixed(4)]);
+  const tableData3 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) * Math.log(d.n))).toFixed(4)]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Expected Height of a Randomly Built Binary Search Tree</h1>
+      <GraphTable
+        traceData={trace1}
+        tableData={tableData1}
+        title="h(n) / 2nlg(n) versus n"
+        xTitle="n"
+        yTitle="h(n) / 2nlg(n)"
+      />
+      <GraphTable
+        traceData={trace2}
+        tableData={tableData2}
+        title="h(n) / 2n√n versus n"
+        xTitle="n"
+        yTitle="h(n) / 2n√n)"
+      />
+      <GraphTable
+        traceData={trace3}
+        tableData={tableData3}
+        title="(n) / 2nlg(n) versus n"
+        xTitle="n"
+        yTitle="(n) / 2nlg(n)"
+      />
     </div>
   );
 }
