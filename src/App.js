93033ea3b195159194fe5360094709952e51a659
Chris <f.chrisgoodson@icloud.com>
2023-03-07 13:52:22 -0600

    updated functions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   const trace1 = {
     x: data.map(d => d.n),
-    y: data.map(d => d.height / (2 * Math.log(d.n))),
+    y: data.map(d => d.height / (2 * Math.log(d.n) / Math.log(2))),
     type: 'scatter',
     name: 'h(n) / 2lg(n)',
     
@@ -16,21 +16,21 @@ function App() {
 
   const trace2 = {
     x: data.map(d => d.n),
-    y: data.map(d => d.height / ((2 * d.n) ** 0.5)),
+    y: data.map(d => d.height / ((2 * d.n) * Math.sqrt(d.n))),
     type: 'scatter',
     name: 'h(n) / 2n√n)',
   };
 
   const trace3 = {
     x: data.map(d => d.n),
-    y: data.map(d => d.height / ((2 * d.n) * Math.log(d.n))),
+    y: data.map(d => d.height / ((2 * d.n) * Math.log(d.n) / Math.log(2))),
     type: 'scatter',
     name: 'h(n) / 2nlg(n)',
   };
 
-  const tableData1 = data.map(d => [d.n, d.height, (d.height / (2 * Math.log(d.n))).toFixed(4)]);
-  const tableData2 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) ** 0.5)).toFixed(4)]);
-  const tableData3 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) * Math.log(d.n))).toFixed(4)]);
+  const tableData1 = data.map(d => [d.n, d.height, (d.height / (2 * Math.log(d.n) / Math.log(2))).toFixed(4)]);
+  const tableData2 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) * Math.sqrt(d.n))).toFixed(4)]);
+  const tableData3 = data.map(d => [d.n, d.height, (d.height / ((2 * d.n) * Math.log(d.n) / Math.log(2))).toFixed(4)]);
 
   return (
     <div className="container">
